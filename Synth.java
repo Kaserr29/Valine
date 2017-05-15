754151ab035b47d1bf78ccece6c47559062a8899
C_orentin <29.kaserr@gmail.com>
2017-05-15 11:37:14 +0200

    Synthé fonctionnel
    Possible de faire jouer une séquence de notes au synthé
    JavaDoc commencée
    TO DO : choix de l'instrument

--- Synth.java
+++ Synth.java
@@ -21,8 +21,88 @@
  * 
  */
 
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Synthesizer;
+import javax.sound.midi.MidiChannel;
+import javax.sound.midi.MidiUnavailableException;
 
 public class Synth{
+	int tempo;
+	Sequence Seq;
+	int[][] sequence;
+	int dureeNote;
+
+	public Synth(Sequence s, int t){
+		this.tempo = t;
+		this.dureeNote = calculDureeNote(this.tempo);
+		this.sequence = s.sequence;
+	}
 	
+/**
+ * /!\TEMPORAIRE/!\ Joue la sequence en bouble 
+ */
+	public void jouer(){
+		while(true){
+			iterSequence(this.sequence);
+		}
+	}
+/**
+ * Calcule la durée d'une note en fonction du tempo
+ * @param t tempo en bpm
+ * @return d durée d'une note en ms
+ */
+	public int calculDureeNote(int t){
+		int d = (int)((60.0/(double)t)*1000);
+		System.out.println("TEMPO : "+ d); 
+		return d;
+	}
+/**
+ * Parcours la sequence de notes et la fait jouer par le synthetiseur de JAVA
+ * @param seq Tableau de la séquence précédement généré
+ */
+	public void iterSequence(int[][] seq){
+		for(int i = 0; i<seq.length; i++){
+			for(int j = 0; j<seq[i].length; j++){
+				jouerNote(seq[i][j]);
+				System.out.println("Playing : "+ seq[i][j]);
+			}
+		}
+	}
+/**
+ * Joue la note avec le Synthétiseur JAVA
+ * @param note Note à jouer
+ */
+	public void jouerNote(int note){
+		Synthesizer	synth = null;
+
+		try{
+			synth = MidiSystem.getSynthesizer();
+		}
+		catch(MidiUnavailableException e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		try{
+			synth.open();
+		}
+		catch (MidiUnavailableException e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		MidiChannel[] channels = synth.getChannels();
+		MidiChannel	channel = channels[0];
+		channel.noteOn(note, 127);
+
+		try{
+			Thread.sleep(dureeNote);
+		}
+		catch (InterruptedException e)
+		{}
+		channel.noteOff(note);
+		synth.close();
+
+	}
 }
 
