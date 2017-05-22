e998dd6c66797897f8a91ddae7a7e37548a9c824
Thomiche <thomas.lefebvre@insa-lyon.fr>
2017-05-22 11:36:48 +0200

    Gamme
    Normalement c'est bon wallah putain en vrai ça me casse les burneszer

--- Gamme.java
+++ Gamme.java
@@ -1,7 +1,7 @@
 /*
  * Gamme.java
  * 
- * Copyright 2017 Unknown <tlefebvre@pc107-043-24>
+ * Copyright 2017 Lucas Corentin <clucas@pc107-043-07>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,10 +20,42 @@
  * 
  * 
  */
+// Groupe 3 
+// Projet "Valine", par B. SORIN, C. LUCAS, T.LEFEBVRE
+// rédacteur de la  classe : T. LEFEBVRE
 
 
 public class Gamme {
+	static int [][] init = { {0,2,4,5,7,9,11},{0,2,3,5,7,8,10} };
+	int [] s;
+	int mode;
+	int tonalite;
+	int octave;
 	
+	public Gamme  (int m, int t, int o)	{
+		mode = m;
+		tonalite = t;
+		octave = o;
+	}
+	
+	public int [] generator ()	{
+		
+		for (int i = 0 ; i < s.length ; i++) {
+			if (mode == 0)	{
+				s[i] = init [0][i];
+			} else if (mode == 1)	{
+				s[i] = init [1][i];
+			} 
+			s[i] += tonalite;
+			s[i] += octave;
+		}
+	return s;	
+	}	
+	
+	public String toString()	{
+		return "Mode : "+this.mode+"; Tonalite : "+this.tonalite+"; Octave ; "+this.octave ;
+	}
+		
 	
 }
 
